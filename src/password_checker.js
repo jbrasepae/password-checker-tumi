591bbc94ab7d7713bb759f3ba4696b05b545a11c
Jeanette <jbrasepae@gmail.com>
2020-05-13 17:53:45 +0200

    added function

--- src/password_checker.js
+++ src/password_checker.js
@@ -1,7 +1,16 @@
+const password = "Tumie@27"
+
 function passwordIsValid(password){
-
+    //const regex = /Tumie/ig
+    //const regex = /\d/g
+    const regex = /(?=.*[a-z])(?=.*[\d])(?=.*[A-Z])(?=.*[@!#$%^&*?]).{8,}/g
+    return password.match(regex)
+    //return regex.test(password) 
+    //return regex.test(password) 
 }
+//console.log(passwordIsValid("Tumie@27"))
+module.exports = {passwordIsValid}
 
-function passwordIsOk(password){
+// function passwordIsOk(password){
     
-}
+// }
