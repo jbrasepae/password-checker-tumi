7f0661a39a9b137aca403c6f762013fd129a65e1
Jeanette <jbrasepae@gmail.com>
2020-05-13 17:52:47 +0200

    added test function

--- spec/password_is_valid_spec.js
+++ spec/password_is_valid_spec.js
@@ -1,1 +1,15 @@
 const {passwordIsValid} = require("../src/password_checker")
+
+const password = "Tumie@27"
+
+describe('passwordIsValid',function(){
+    it('should exist', function(){
+        expect(passwordIsValid(password)).toEqual(['Tumie@27'])
+    })
+    it('should be longer than 8 characters', function(){
+        expect(passwordIsValid(password)).toEqual(['Tumie@27'])
+    })
+    it('should have atleast one lowecase letter', function(){
+        expect(passwordIsValid(password)).toEqual(['?=.*[a-z]'])
+    })
+})
