545b439cba45d19eeef0b2b2709c9291f5318e0a
Jeanette <jbrasepae@gmail.com>
2020-05-15 02:14:53 +0200

    completed spec for passwordisvalid

--- spec/password_is_valid_spec.js
+++ spec/password_is_valid_spec.js
@@ -1,15 +1,22 @@
 const {passwordIsValid} = require("../src/password_checker")
 
-const password = "Tumie@27"
-
-describe('passwordIsValid',function(){
-    it('should exist', function(){
-        expect(passwordIsValid(password)).toEqual(['Tumie@27'])
+describe("passwordIsValid",function(){
+    it('should check if password exist', function(){
+        expect(function() {passwordIsValid(null)}).toThrow(new Error("password should exist"))
     })
     it('should be longer than 8 characters', function(){
-        expect(passwordIsValid(password)).toEqual(['Tumie@27'])
+        expect(function() {passwordIsValid("tumi")}).toThrow(new Error("it should be longer than 8 character"))
     })
-    it('should have atleast one lowecase letter', function(){
-        expect(passwordIsValid(password)).toEqual(['?=.*[a-z]'])
+    it('should have atleast one lowercase letter', function(){
+        expect(function() {passwordIsValid("TUMIIIIII")}).toThrow(new Error("it should have atleast one lowercase letter"))
+    })
+    it('should have atleast one uppercase letter', function(){
+        expect(function() {passwordIsValid("tummiiie")}).toThrow(new Error("it should have atleast one uppercase letter"))
+    })
+    it('should have atleast one digit', function(){
+        expect(function() {passwordIsValid("Tumiee@y")}).toThrow(new Error("it should have atleast one digit"))
+    })
+    it('should have atleast one special character', function(){
+        expect(function() {passwordIsValid("Tumiee27")}).toThrow(new Error("it should have atleast one special character"))
     })
 })
